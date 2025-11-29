8c89fc7e6d586c24b38deae0ccfe8c4c23c97d03
Patrick McKee <pwmckee1@gmail.com>
2025-11-29 11:08:17 -0500

    Integrate Google Secret Manager for Firebase credentials retrieval

--- AGT.GalacticArchives.Core/Models/Application/ApplicationSettings.cs
+++ AGT.GalacticArchives.Core/Models/Application/ApplicationSettings.cs
@@ -1,9 +1,12 @@
-﻿namespace AGT.GalacticArchives.Core.Models.AppSettings;
+﻿using AGT.GalacticArchives.Core.Models.AppSettings;
+
+namespace AGT.GalacticArchives.Core.Models.Application;
 
 public class ApplicationSettings
 {
-    public string[] AllowedOrigins { get; set; } = null!;
+    public string[] AllowedOrigins { get; set; } = [];
 
     public FirebaseSettings Firebase { get; set; } = new();
     public bool EnableDeveloperErrors { get; set; }
+    public string? GoogleCloudProjectId { get; set; }
 }
