34051129aadc2d036bdd25288e4e72c0b48b22d1
Patrick McKee <pwmckee1@gmail.com>
2025-12-02 14:04:44 -0500

    Added missing service for POI

---
+++ AGT.GalacticArchives.Services/Services/GameData/Interfaces/IPointOfInterestService.cs
@@ -0,0 +1,15 @@
+using AGT.GalacticArchives.Core.Models.Requests;
+using AGT.GalacticArchives.Core.Models.Responses;
+
+namespace AGT.GalacticArchives.Services.Services.GameData.Interfaces;
+
+public interface IPointOfInterestService
+{
+    Task<PointOfInterestResponse?> GetPointOfInterestByIdAsync(Guid pointOfInterestId);
+
+    Task<HashSet<PointOfInterestResponse>> GetPointOfInterestsAsync(PointOfInterestRequest request);
+
+    Task<PointOfInterestResponse> UpsertPointOfInterestAsync(PointOfInterestRequest pointOfInterest);
+
+    Task DeletePointOfInterestAsync(Guid pointOfInterestId);
+}
