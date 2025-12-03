3b5dd2c54b095ed215dee6ab0cef22079d907dc7
Patrick McKee <pwmckee1@gmail.com>
2025-12-03 00:48:39 -0500

    Cleaned up extensions

--- AGT.GalacticArchives.Core/Attributes/DisplayAttributeEnumConverter.cs
+++ AGT.GalacticArchives.Core/Attributes/DisplayAttributeEnumConverter.cs
@@ -1,8 +1,8 @@
-﻿namespace AGT.GalacticArchives.Core.Attributes;
-
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using AGT.GalacticArchives.Core.Extensions;
+
+namespace AGT.GalacticArchives.Core.Attributes;
 
 public class DisplayAttributeEnumConverter : JsonConverterFactory
 {
@@ -17,25 +17,29 @@ public class DisplayAttributeEnumConverter : JsonConverterFactory
         return (JsonConverter)Activator.CreateInstance(converterType)!;
     }
 
-    private class DisplayAttributeEnumConverterInner<T> : JsonConverter<T> where T : struct, Enum
+    private sealed class DisplayAttributeEnumConverterInner<T> : JsonConverter<T>
+        where T : struct, Enum
     {
         public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             string? value = reader.GetString();
 
             if (string.IsNullOrEmpty(value))
+            {
                 throw new JsonException($"Empty string is not a valid value for {typeToConvert.Name}");
+            }
 
             // Try exact enum name match first
             if (Enum.TryParse<T>(value, true, out var result))
+            {
                 return result;
+            }
 
             // Try Display attribute description match using your extension
             var fromDescription = value.GetValueFromDescription<T>();
-            if (fromDescription.HasValidValue())
-                return fromDescription;
-
-            throw new JsonException($"'{value}' is not a valid value for {typeToConvert.Name}");
+            return fromDescription.HasValidValue()
+                ? fromDescription
+                : throw new JsonException($"'{value}' is not a valid value for {typeToConvert.Name}");
         }
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
--- AGT.GalacticArchives.Core/Extensions/AttributeExtensions.cs
+++ AGT.GalacticArchives.Core/Extensions/AttributeExtensions.cs
@@ -1,106 +1,121 @@
-﻿namespace AGT.GalacticArchives.Core.Extensions;
-
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using AGT.GalacticArchives.Core.Attributes;
 using CsvHelper.Configuration.Attributes;
 
+namespace AGT.GalacticArchives.Core.Extensions;
+
 public static class AttributeExtensions
 {
-    /// <summary>
-    ///     Validates attribute(s) on a property
-    /// </summary>
-    /// <param name="objectToValidate">The object to validate</param>
-    /// <param name="propertyName">The name of the property to validate</param>
-    /// <param name="propertyToValidate">The property on the object to validate</param>
-    public static List<ValidationResult> ValidateProperty(
-        this object objectToValidate,
-        string propertyName,
-        object? propertyToValidate)
-    {
-        var validationResults = new List<ValidationResult>();
-        var validationContext = new ValidationContext(objectToValidate, null, null) { MemberName = propertyName };
-        Validator.TryValidateProperty(propertyToValidate, validationContext, validationResults);
-
-        return validationResults;
-    }
-
-    /// <summary>
-    /// Get attribute from property - null if attribute is not found
-    /// </summary>
+    /// <param name="property"></param>
     /// <typeparam name="T">typeof class containing attribute</typeparam>
-    /// <typeparam name="TAttribute">typeof attribute</typeparam>
-    /// <param name="property"></param>
-    /// <returns></returns>
-    public static TAttribute? GetAttribute<T, TAttribute>(this T property)
-        where TAttribute : Attribute
+    extension<T>(T property)
     {
-        var type = typeof(T);
-        var fieldInfo = type.GetField(property?.ToString()!);
-        return fieldInfo != null
-            ? fieldInfo.GetCustomAttribute<TAttribute>()
-            : null;
+        /// <summary>
+        /// Get attribute from property - null if attribute is not found
+        /// </summary>
+        /// <typeparam name="TAttribute">typeof attribute</typeparam>
+        /// <returns></returns>
+        public TAttribute? GetAttribute<TAttribute>()
+            where TAttribute : Attribute
+        {
+            var type = typeof(T);
+            var fieldInfo = type.GetField(property?.ToString()!);
+            return fieldInfo != null
+                ? fieldInfo.GetCustomAttribute<TAttribute>()
+                : null;
+        }
+
+        public string? GetSecondaryDescription()
+        {
+            var secondaryDescriptionAttribute = property.GetAttribute<T, SecondaryDescriptionAttribute>();
+            return secondaryDescriptionAttribute?.SecondaryDescription;
+        }
+
+        /// <summary>
+        ///     Get Localized Description from Display, Description, or Name Attribute on property
+        /// </summary>
+        /// <returns></returns>
+        public string GetDescription()
+        {
+            var description = property?.ToString()!;
+            return GetDescription<T>(description);
+        }
     }
 
-    public static string? GetSecondaryDescription<T>(this T property)
+    /// <param name="objectToValidate">The object to validate</param>
+    extension(object objectToValidate)
     {
-        var secondaryDescriptionAttribute = property.GetAttribute<T, SecondaryDescriptionAttribute>();
-        return secondaryDescriptionAttribute?.SecondaryDescription;
+        /// <summary>
+        ///     Validates attribute(s) on a property
+        /// </summary>
+        /// <param name="propertyName">The name of the property to validate</param>
+        /// <param name="propertyToValidate">The property on the object to validate</param>
+        public List<ValidationResult> ValidateProperty(
+            string propertyName,
+            object? propertyToValidate)
+        {
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(objectToValidate, null, null) { MemberName = propertyName };
+            Validator.TryValidateProperty(propertyToValidate, validationContext, validationResults);
+
+            return validationResults;
+        }
     }
 
-    /// <summary>
-    ///     Get Localized Description from Display, Description, or Name Attribute on property
-    /// </summary>
-    /// <typeparam name="T">typeof class or enum</typeparam>
-    /// <param name="property"></param>
-    /// <returns></returns>
-    public static string GetDescription<T>(this T property)
-    {
-        var description = property?.ToString()!;
-        return GetDescription<T>(description);
-    }
-
-    /// <summary>
-    ///      Get Localized Description from Display, Description, or Name Attribute on property
-    /// </summary>
-    /// <typeparam name="T">typeof class or enum containing property</typeparam>
     /// <param name="propertyName"></param>
-    /// <returns></returns>
-    public static string GetDescription<T>(this string propertyName)
+    extension(string propertyName)
     {
-        string description = propertyName;
-        var type = typeof(T);
-        var fieldInfo = type.GetField(description);
-        if (fieldInfo != null) description = fieldInfo.GetDescription();
+        /// <summary>
+        ///      Get Localized Description from Display, Description, or Name Attribute on property
+        /// </summary>
+        /// <typeparam name="T">typeof class or enum containing property</typeparam>
+        /// <returns></returns>
+        public string GetDescription<T>()
+        {
+            string description = propertyName;
+            var type = typeof(T);
+            var fieldInfo = type.GetField(description);
+            if (fieldInfo != null)
+            {
+                description = fieldInfo.GetDescription();
+            }
 
-        return description;
+            return description;
+        }
     }
 
-    /// <summary>
-    ///     Get Localized Description from Display, Description, or Name Attribute on fieldInfo
-    /// </summary>
     /// <param name="fieldInfo"></param>
-    /// <returns></returns>
-    public static string GetDescription(this FieldInfo fieldInfo)
+    extension(FieldInfo fieldInfo)
     {
-        var displayAttribute = fieldInfo.GetCustomAttribute<DisplayAttribute>();
-        var descriptionAttribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>();
-        var nameAttribute = fieldInfo.GetCustomAttribute<NameAttribute>();
-        return GetDescription(displayAttribute, descriptionAttribute, nameAttribute, fieldInfo.Name);
+        /// <summary>
+        ///     Get Localized Description from Display, Description, or Name Attribute on fieldInfo
+        /// </summary>
+        /// <returns></returns>
+        public string GetDescription()
+        {
+            var displayAttribute = fieldInfo.GetCustomAttribute<DisplayAttribute>();
+            var descriptionAttribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>();
+            var nameAttribute = fieldInfo.GetCustomAttribute<NameAttribute>();
+            return GetDescription(displayAttribute, descriptionAttribute, nameAttribute, fieldInfo.Name);
+        }
     }
 
-    /// <summary>
-    ///     Get Localized Description from Display, Description, or Name Attribute on propertyInfo
-    /// </summary>
     /// <param name="propertyInfo"></param>
-    /// <returns></returns>
-    public static string GetDescription(this PropertyInfo propertyInfo)
+    extension(PropertyInfo propertyInfo)
     {
-        var displayAttribute = propertyInfo.GetCustomAttribute<DisplayAttribute>();
-        var descriptionAttribute = propertyInfo.GetCustomAttribute<DescriptionAttribute>();
-        var nameAttribute = propertyInfo.GetCustomAttribute<NameAttribute>();
-        return GetDescription(displayAttribute, descriptionAttribute, nameAttribute, propertyInfo.Name);
+        /// <summary>
+        ///     Get Localized Description from Display, Description, or Name Attribute on propertyInfo
+        /// </summary>
+        /// <returns></returns>
+        public string GetDescription()
+        {
+            var displayAttribute = propertyInfo.GetCustomAttribute<DisplayAttribute>();
+            var descriptionAttribute = propertyInfo.GetCustomAttribute<DescriptionAttribute>();
+            var nameAttribute = propertyInfo.GetCustomAttribute<NameAttribute>();
+            return GetDescription(displayAttribute, descriptionAttribute, nameAttribute, propertyInfo.Name);
+        }
     }
 
     private static string GetDescription(
@@ -113,19 +128,27 @@ public static class AttributeExtensions
 
         // Get localized Description from Display attribute
         if (displayAttribute != null)
+        {
             description = displayAttribute.GetDescription()!;
+        }
 
         // Get Description from Description attribute
         else if (descriptionAttribute != null)
+        {
             description = descriptionAttribute.Description;
+        }
 
         // Get Description from Name attribute
         else if (nameAttribute != null)
+        {
             description = nameAttribute.Names.FirstOrDefault() ?? string.Empty;
+        }
 
         // Get FieldInfo Name if there are no attributes
         else
+        {
             description = defaultValue;
+        }
 
         return description;
     }
